e633f7e9b6b6caf025074a4f9154d2353d76bdac
fercz7 <fercespedes2@gmail.com>
2020-11-09 10:29:27 -0600

    inlcuir los dias 1
    Dia de plantacion

--- pasar base a nuestro formato.R
+++ pasar base a nuestro formato.R
@@ -6,25 +6,25 @@ info[1, 2] = "1.1"
 
 H = c()
 for( i in 1:nrow( info)){
-  k = c( as.numeric( info[i,3]), as.numeric( info[i,4]), as.numeric( info[i,5]), as.numeric( info[i, 7]))
+  k = c( 0, as.numeric( info[i,3]), as.numeric( info[i,4]), as.numeric( info[i,5]), as.numeric( info[i, 7]) )
   H = append(H, k)
 }
 
 D = c()
 for( i in 1:nrow( info)){
-  k = c( as.numeric( info[i,9]), as.numeric( info[i,10]), as.numeric( info[i,11]), as.numeric( info[i, 13]))
+  k = c( 0, as.numeric( info[i,9]), as.numeric( info[i,10]), as.numeric( info[i,11]), as.numeric( info[i, 13]))
   D = append(D, k)
 }
 
-base = data.frame( tratamiento = as.factor( rep( info$Tratamiento, each = 4) ),
-                   medida = as.factor( rep( info$Medida, each = 4) ),
+base = data.frame( tratamiento = as.factor( rep( info$Tratamiento, each = 5) ),
+                   medida = as.factor( rep( info$Medida, each = 5) ),
                    altura = H,
                    diametro = D,
-                   n_med = rep(c(28*1, 28*2, 28*3, 28*5), 10*4 ) 
+                   dia = rep(c(1, 28*1, 28*2, 28*3, 28*5), 10*4 ) 
 )
 
 base = base %>% replace_na( list( altura = 0,
                                   diametro = 0
 ) )
-
+View( base )
 save( base, file = "germinacion.Rdata")
