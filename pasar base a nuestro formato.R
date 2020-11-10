2cc4745c7d8f9f378bfd2b8c2d8606a6b6c953e7
fercz7 <fercespedes2@gmail.com>
2020-11-10 10:10:52 -0600

    Corregir la base

--- pasar base a nuestro formato.R
+++ pasar base a nuestro formato.R
@@ -2,25 +2,24 @@
 library(tidyr)
 library(dplyr)
 info = openxlsx::read.xlsx("Experimento para trabajar.xlsx", sheet = "datos"); 
-info[1, 2] = "1.1"
-
+info[1, 3] = "1.1"
+View(info)
 H = c()
-for( i in 1:nrow( info)){
-  k = c( 0, as.numeric( info[i,3]), as.numeric( info[i,4]), as.numeric( info[i,5]), as.numeric( info[i, 7]) )
-  H = append(H, k)
-}
-
 D = c()
 for( i in 1:nrow( info)){
-  k = c( 0, as.numeric( info[i,9]), as.numeric( info[i,10]), as.numeric( info[i,11]), as.numeric( info[i, 13]))
-  D = append(D, k)
+  k = c(  as.numeric( info[i,3+1]), as.numeric( info[i,4+1]), as.numeric( info[i,5+1]), as.numeric( info[i, 7+1]) )
+  H = append(H, k)
+  j = c(  as.numeric( info[i,9+1]), as.numeric( info[i,10+1]), as.numeric( info[i,11+1]), as.numeric( info[i, 13+1]))
+  D = append(D, j)
 }
 
-base = data.frame( tratamiento = as.factor( rep( info$Tratamiento, each = 5) ),
-                   medida = as.factor( rep( info$Medida, each = 5) ),
+
+base = data.frame( riego = as.factor( rep( info$Riego, each = 4) ),
+                   hongos = as.factor( rep( info$Hongos, each = 4) ),
+                   planta = as.factor( rep( info$Medida, each = 4) ),
                    altura = H,
                    diametro = D,
-                   dia = rep(c(1, 28*1, 28*2, 28*3, 28*5), 10*4 ) 
+                   dia = rep(c(28*1, 28*2, 28*3, 28*5), 10*4 ) 
 )
 
 base = base %>% replace_na( list( altura = 0,
